bda2cf2341483c6762cad55a2be3294ff7330fc0
Ilya <hatake18@bk.ru>
2019-12-02 23:38:28 +0300

    частичная верстка блока преимуществ и верстка всех версий о компании

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var MOBILE_WIDTH = 768;
+var TABLET_WIDTH = 1023;
 var adviceButton = document.querySelector('.promo__link--advice');
+var changingText = document.querySelector('.about-company__js-text-change');
 
-var changeText = function () {
+var changeTextPromo = function () {
   if (adviceButton) {
     if (document.body.clientWidth <= MOBILE_WIDTH) {
       adviceButton.innerHTML = 'Бесплатная консультация';
@@ -13,6 +15,17 @@ var changeText = function () {
   }
 };
 
+var changeTextAboutCompany = function () {
+  if (changingText) {
+    if (document.body.clientWidth <= TABLET_WIDTH) {
+      changingText.innerHTML = 'Наши поставщики - мировые производители электронных компонентов: OSRAM, CREE, HOLGLITRONIC, REFOND. Печатные платы и комплектующие Service Devices применяются на предприятиях Российских Железных Дорог..';
+    } else {
+      changingText.innerHTML = 'Наши поставщики - мировые производители электронных компонентов: OSRAM, CREE, HOLGLITRONIC, REFOND. Печатные платы и комплектующие Service Devices применяются на предприятиях Российских Железных Дорог (РЖД), РоссАвтоПрома (ВАЗ, АвтоГАЗ), МинАтома, СпецМедТехники. Среди наших клиентов крупнейшие Производители светотехники России.';
+    }
+  }
+};
+
 window.addEventListener('resize', function () {
-  changeText();
+  changeTextPromo();
+  changeTextAboutCompany();
 });
