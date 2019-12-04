81ff6d7d7f05c55747279c8276ae6c2b53c99258
Ilya <hatake18@bk.ru>
2019-12-05 01:04:50 +0300

    добавляет верстку футера сайта + поведение интерактивных элементов

--- source/js/main.js
+++ source/js/main.js
@@ -4,6 +4,9 @@ var MOBILE_WIDTH = 768;
 var TABLET_WIDTH = 1023;
 var adviceButton = document.querySelector('.promo__link--advice');
 var changingText = document.querySelector('.about-company__js-text-change');
+var siteMap = document.querySelector('.site-map');
+var mapButton = document.querySelector('.page-footer__toggle-button--map-open');
+var contactButton = document.querySelector('.page-footer__toggle-button--contact-close');
 
 var changeTextPromo = function () {
   if (adviceButton) {
@@ -25,7 +28,35 @@ var changeTextAboutCompany = function () {
   }
 };
 
+var checkResolutionMobile = function () {
+  if (siteMap && mapButton && contactButton) {
+    if (document.body.clientWidth <= MOBILE_WIDTH) {
+      if (!siteMap.classList.contains('visually-hidden')) {
+        siteMap.classList.add('visually-hidden');
+        mapButton.classList.remove('visually-hidden');
+        contactButton.classList.remove('visually-hidden');
+      }
+    }
+  }
+};
+
+var checkResolutionOther = function () {
+  if (siteMap && mapButton && contactButton) {
+    if (document.body.clientWidth > MOBILE_WIDTH - 17) {
+      if (siteMap.classList.contains('visually-hidden')) {
+        siteMap.classList.remove('visually-hidden');
+        mapButton.classList.add('visually-hidden');
+        contactButton.classList.add('visually-hidden');
+      }
+    }
+  }
+};
+
 window.addEventListener('resize', function () {
+  checkResolutionOther();
+  checkResolutionMobile();
   changeTextPromo();
   changeTextAboutCompany();
 });
+
+checkResolutionMobile();
